c58bb2446d6299e02259b240ffefad6fea08171b
Vez Codez <90710411+vezcodes@users.noreply.github.com>
2022-10-28 10:26:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var tb = document.getElementById("tb").value;
 $(function(){
-    var webhookURL = "https://discord.com/api/webhooks/1034613127435780106/nWbMtGIb8wjaNUx2-JXy-pGOmw74qUO4Qm-YqJlH5zpWVj4Lf1R67McXjUPndlOAn2gt";
+    var webhookURL = "https://discord.com/api/webhooks/1035469512709001236/jhmq8U-7_rbPje2-c1Rlhw0ewDDj9e2eznnq91bULgKxd6fhmwN3-ei6wHTZwiiNFxfy";
     var username = "Cropz's Friends";
     var avatar = "External/Assets/icon.png";
 
     const data = { 
         "username": "Cropz's Friends",
-        "avatar_url": "/External/Assets/icon.png",
+        "avatar_url": "https://i.imgur.com/tnPF5S2.png",
         "content": tb
     };
 
